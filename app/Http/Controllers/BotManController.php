376b49a3f45cf0f8ec9b5c4bcf7e3525c67b3a35
ALANCRUZ96942 <alan96942@gmail.com>
2022-03-05 00:52:32 -0600

    portada y bot

---
+++ app/Http/Controllers/BotManController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers;
+   
+use BotMan\BotMan\BotMan;
+use Illuminate\Http\Request;
+use BotMan\BotMan\Messages\Incoming\Answer;
+   
+class BotManController extends Controller
+{
+    /**
+     * Place your BotMan logic here.
+     */
+    public function handle()
+    {
+        $botman = app('botman');
+   
+        $botman->hears('{message}', function($botman, $message) {
+   
+            if ($message == 'Denunciar') {
+                $this->askName($botman);
+            }
+            
+            else{
+                $botman->reply("write 'hi' for testing...");
+            }
+   
+        });
+   
+        $botman->listen();
+    }
+   
+    /**
+     * Place your BotMan logic here.
+     */
+    public function askName($botman)
+    {
+        $botman->ask('Hola, qué tipo de denuncia', function(Answer $answer) {
+   
+            $name = $answer->getText();
+   
+            $this->say('Procesando denuncia '.$name);
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BotManController;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +19,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::match(['get', 'post'], '/botman', [BotManController::class, 'handle']);
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::get('/quienes', function () {
+    return view('welcome');
+})->name('quienes');;
+
+Route::get('/creditos', function () {
+    return view('welcome');
+})->name('creditos');;
+
+Route::get('/estadisticas', function () {
+    return view('welcome');
+})->name('estadisticas');;
